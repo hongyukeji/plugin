6cc075bfdb061c7f4cf774ba86316357f5815e76
shadow <admin@hongyuvip.com>
2019-10-08 19:02:02 +0800

    Initial commit

--- src/Plugin.php
+++ src/Plugin.php
@@ -116,4 +116,33 @@ class Plugin extends \Hongyukeji\PluginPackage\Package
 
         return $this;
     }
+
+    /**
+     * Delete plugin
+     *
+     * @return  \Hongyukeji\Plugin\Plugin
+     */
+    public function remove()
+    {
+        $dirName = $this->getDir();
+        $this->delDirAndFile($dirName);
+        return $this;
+    }
+
+    public function delDirAndFile($dirName)
+    {
+        if ($handle = opendir($dirName)) {
+            while (false !== ($item = readdir($handle))) {
+                if ($item != "." && $item != "..") {
+                    if (is_dir("$dirName/$item")) {
+                        $this->delDirAndFile("$dirName/$item");
+                    } else {
+                        unlink("$dirName/$item");
+                    }
+                }
+            }
+            closedir($handle);
+            rmdir($dirName);
+        }
+    }
 }
