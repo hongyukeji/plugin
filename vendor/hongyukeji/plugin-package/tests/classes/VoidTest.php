8baeed3394b49443b00ceb97dfad20ec9bbb4bb4
shadow <admin@hongyuvip.com>
2019-10-08 10:21:17 +0800

    Initial commit

--- vendor/hongyukeji/plugin-package/tests/classes/VoidTest.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-class VoidTest extends PHPUnit_Framework_TestCase
-{
-	public function testForge()
-	{
-		$this->assertInstanceOf('Hongyukeji\Package\Void', \Hongyukeji\Package\Void::forge());
-	}
-}
