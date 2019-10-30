cd11c43f49d3680f84b258ddef5b1cb8b9056e9b
shadow <admin@hongyuvip.com>
2019-10-30 18:39:31 +0800

    Initial commit

--- src/PluginServiceProvider.php
+++ src/PluginServiceProvider.php
@@ -9,31 +9,50 @@ class PluginServiceProvider extends ServiceProvider
 {
     public function boot()
     {
+        $config_path = __DIR__ . '/../config/plugins.php';
+        $this->mergeConfigFrom($config_path, 'plugins');
         $this->publishes([
-            __DIR__ . '/../config/plugins.php' => config_path('plugins.php'),
+            $config_path => config_path('plugins.php'),
         ], 'config');
 
-        if (class_exists('Hongyukeji\Plugin\Loader')){
+        if (class_exists('Hongyukeji\Plugin\Loader')) {
             $loader = Loader::forge()->addDir(config('plugins.directory', base_path('plugins')));
             foreach ($loader->getAll() as $plugin) {
+                // 判断插件状态是否启用
                 if ($plugin->getConfig('extra.status')) {
+                    $path = str_finish($plugin->getDir(), '/') . "src";
+                    if (is_dir($path)) {
+                        $files = $this->getPathFiles($path);
+                        foreach ($files as $file) {
+                            $file_path = $path . $file;
+                            if (file_exists($file_path) && ends_with($file_path, '.php')) {
+                                include_once $file_path;
+                            }
+                        }
+                    }
                     $plugin->execute();
                 }
             }
         }
     }
 
-    /*
-     * 插件扫描加载机制
-     */
-    public function plugin()
+    public function tree(& $array_files, $directory, $dir_name = '')
     {
-        $loader = Loader::forge()->addDir(config('plugins.directory', base_path('plugins')));
-        $enabled_plugins = config('plugins.enabled_plugins', []);
-        foreach ($loader->getAll() as $plugin) {
-            if (in_array($plugin->getConfig('name'), $enabled_plugins)) {
-                $plugin->execute();
+        $mydir = dir($directory);
+        while ($file = $mydir->read()) {
+            if ((is_dir("$directory/$file")) AND ($file != ".") AND ($file != "..")) {
+                $this->tree($array_files, "$directory/$file", "$dir_name/$file");
+            } else if (($file != ".") AND ($file != "..")) {
+                $array_files[] = "$dir_name/$file";
             }
         }
+        $mydir->close();
+    }
+
+    public function getPathFiles($path)
+    {
+        $array_files = array();
+        $this->tree($array_files, $path);
+        return $array_files;
     }
 }
